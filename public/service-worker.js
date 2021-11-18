5d43a3c7bb8d1490fe86cad81880b5f1c2e038db
alonzofroman <alonzofroman@gmail.com>
2021-11-18 15:07:06 -0800

    Several fixes to service worker with wait until functions and failed fetches

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,42 +13,50 @@ const FILES_TO_CACHE = [
     'icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', function (evt) {
-    evt.WaitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+self.addEventListener('install', (evt) => {
+    evt.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
 
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function (evt) {
-    evt.waitUntil(caches.keys().then(keyList => {
-        return Promise.all(keyList.map(key => {
-            if (key != CACHE_NAME && key != DATA_CACHE_NAME) {
-                console.log ('Removing old data cache', key);
-                return caches.delete(key);
-            }
-        }))
-    }));
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', function (evt) {
-    if (evt.request.url.includes('api/transaction')) {
-        evt.respondWith(caches.open(DATA_CACHE_NAME).then(cache => {
-            return fetch(evt.request).then(response => {
-                if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                    return response;
-                }
-            }).catch(err => {
-                return cache.match(evt.request);
-            })
-        }));
-        return;
-    };
-    evt.respondWith(caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-            return response || fetch(evt.request);
+self.addEventListener('activate', (evt) => {
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+    evt.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
         })
-    }))
-});
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
 
+self.addEventListener('fetch', (evt) => {
+    if (evt.request.url.startsWith(self.location.origin)) {
+      evt.respondWith(
+        caches.match(evt.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(CACHE_NAME).then((cache) => {
+            return fetch(evt.request).then((response) => {
+              return cache.put(evt.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+  });
